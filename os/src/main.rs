0d1a5b7df23c25d8dd5365bc0473291212015755
多吃韩66 <15535517032@163.com>
2025-03-21 21:28:53 +0800

    update

--- os/src/main.rs
+++ os/src/main.rs
@@ -71,7 +71,7 @@ fn kernel_log_info() {
         fn boot_stack_top(); // stack top
     }
     logging::init();
-    println!("[kernel] Hello, Dchll!");
+    println!("[kernel] Hello, world!");
     trace!(
         "[kernel] .text [{:#x}, {:#x})",
         stext as usize,
